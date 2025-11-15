d13e7bde4b2455a373a3824d7bc939aad3731eab
Samuel Limbu <Samuel_Limbu@hotmail.com>
2025-11-15 13:48:10 +0000

    Update etl.sql

--- sql/etl.sql
+++ sql/etl.sql
@@ -1,1 +1,10 @@
+DROP TABLE IF EXISTS mart_monthly_revenue;
+CREATE TABLE mart_monthly_revenue AS
+SELECT
+    strftime('%Y-%m', order_purchase_timestamp) AS year_month,
+    SUM(payment_value) AS revenue,
+    COUNT(DISTINCT order_id) AS orders
+FROM orders
+JOIN payments USING(order_id)
+GROUP BY 1;
 
