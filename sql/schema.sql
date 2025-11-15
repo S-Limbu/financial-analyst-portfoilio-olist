7c3a9a1d1dcb50bc838ea3d0d1da43fd4b8d4257
Samuel Limbu <Samuel_Limbu@hotmail.com>
2025-11-15 13:47:49 +0000

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,17 @@
+CREATE TABLE IF NOT EXISTS orders (
+    order_id TEXT PRIMARY KEY,
+    customer_id TEXT,
+    order_status TEXT,
+    order_purchase_timestamp TEXT,
+    delivered_customer_date TEXT,
+    estimated_delivery_date TEXT
+);
+
+CREATE TABLE IF NOT EXISTS payments (
+    order_id TEXT,
+    payment_sequential INTEGER,
+    payment_type TEXT,
+    payment_installments INTEGER,
+    payment_value REAL
+);
 
