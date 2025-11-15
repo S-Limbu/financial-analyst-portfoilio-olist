196a964fd76ec40fafc4687462fbb3556cf614ce
Samuel Limbu <Samuel_Limbu@hotmail.com>
2025-11-15 13:48:18 +0000

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -1,1 +1,9 @@
+-- Top 10 categories by revenue
+SELECT product_category_name, SUM(payment_value) AS revenue
+FROM order_items
+JOIN products USING(product_id)
+JOIN payments USING(order_id)
+GROUP BY 1
+ORDER BY revenue DESC
+LIMIT 10;
 
